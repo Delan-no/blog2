5b4b35c0ca575fe087da3297efc1fe81e2cc0637
Délanno Kotcho <delanktc24@gmail.com>
2024-07-23 19:06:41 +0200

    initialissation du projet de

---
+++ src/views/Home.vue
@@ -0,0 +1,87 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      <p>je suis Délanno kotcho</p>
+      <p>Je suis une page d'accueil</p>
+      <p>Ceci est un exemple de page Vue.js</p>
+      <button >Change Message</button>
+      <p></p>
+      
+    </div>
+  </template>
+  
+  <script>
+
+  </script>
+  
