9c198d5111bd0abe95b5c43b15973638ddde05d4
Délanno Kotcho <delanktc24@gmail.com>
2024-07-24 17:13:16 +0200

    mis en place du composant Header

--- src/components/navbar/Header.vue
+++ src/components/navbar/Header.vue
@@ -15,7 +15,7 @@
         />
         <span
           class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-          >Flowbite</span
+          >BLOG</span
         >
       </a>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
@@ -23,39 +23,34 @@
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
           <li>
-            <a
-              href="#"
+            <router-link to="/"
               class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
               aria-current="page"
-              >Home</a
+              >Accueil</router-link
             >
           </li>
           <li>
-            <a
-              href="#"
+            <router-link to="/"
               class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >About</a
+              >Articles</router-link
             >
           </li>
           <li>
-            <a
-              href="#"
+            <router-link to="/"
               class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >Services</a
+              >Actualités</router-link
             >
           </li>
           <li>
-            <a
-              href="#"
+            <router-link to="/"
               class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >Pricing</a
+              >A propos</router-link
             >
           </li>
           <li>
-            <a
-              href="#"
+            <router-link to="/"
               class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >Contact</a
+              >Contact</router-link
             >
           </li>
         </ul>
