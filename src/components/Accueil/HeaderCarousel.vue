b8cdd50a22aac8dee31c50f39b84a45a30543b42
Délanno Kotcho <delanktc24@gmail.com>
2024-07-30 17:27:19 +0200

    creation du composant HeaderCarousel

---
+++ src/components/Accueil/HeaderCarousel.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <h1 class="text-gray-600 ">
+      22 lois du marketing que vous devez connaître
+    </h1>
+    <p>
+      Il existe 22 lois immuables que tout marketeur devrait connaître par cœur.
+      Elles résistent au temps et restent pertinentes. Pour en lire plus:
+      <a href="https://www.strategemarketing.com/?p=940">https://www.strategemarketing.com</a>
+    </p>
+    <p>Ces 22 lois immuables ont été édictée par Jack Trout et Al Ries. En voici le résumé.</p>
+
+    <ol>
+        <li>
+            <h3>La loi du premier</h3>
+            <p>Mieux vaut être le premier que le meilleur. C'est plus difficile de convaincre les gens qu'on est meilleur lorsqu'on est deuxième. Les gens ne se souviennent que du premier. Vous souvenez-vous du nom du 2ème homme qui a marché sur la lune ?</p>
+        </li>
+        <li>
+            <h3>Loi de la catégorie</h3>
+            <p>Si vous n'êtes pas premier de votre catégorie créez-en une nouvelle dans laquelle vous serez le premier. Autrement dit, initiez votre propre créneau que vous occuperez seul, le premier. Inventez un produit qui répond à la question « le premier quoi ? »</p>
+        </li>
+        <li>
+            <h3>La loi de l'esprit</h3>
+            <p>Ce qui compte c'est d'être le premier dans l'esprit des clients. Il est préférable d'occuper la première place dans l'esprit des gens que d'être premier dans sur le marché. Une fois que les gens auront la croyance que vous êtes le premier, ils ne la remettront plus en cause. Luttez pour être premier dans la tête de vos clients, avant tout.</p>
+        </li>
+        <li>
+            <h3>La loi de la perception</h3>
+            <p>Le marketing est une bataille de perception. Pas de produit. Perception = réalité. Tout le reste n'est qu'illusion. Ce que votre client perçoit est réel à ses yeux.</p>
+        </li>
+        <li>
+            <h3>La loi de la convergence</h3>
+            <p>Concentrez vos activités sur une chose, une seule. Le concept le plus fort consiste à s'approprier un mot dans l'esprit du client. Un seul. Les meilleures marques parviennent à préempter un mot, à tel point qu'ils deviennent des noms communs. Pensez au scotch. Au Sopalin ou aux Pampers.</p>
+        </li>
+        <li>
+            <h3>La loi de l'exclusivité</h3>
+            <p>Deux marques ne peuvent prétendre au même mot dans l'esprit des clients.</p>
+        </li>
+        <li>
+            <h3>La loi de la dualité</h3>
+            <p>À terme, tout marché se réduit à un duel entre deux concurrents. Pensez à la catégorie des colas : Pepsi et Coca-Cola. Deux concurrents. PC et Mac. Deux concurrents. Et ainsi de suite.</p>
+        </li>
+        <li>
+            <h3>La loi des opposés</h3>
+            <p>Si vous voulez prospérer en deuxième position, vous devez établir votre stratégie en fonction du premier. Et opter pour une stratégie opposée. Tentez toujours d'être différent. Pas meilleur, mais différent.</p>
+        </li>
+        <li>
+            <h3>La loi de la division</h3>
+            <p>Avec le temps toutes les catégories se divisent en deux</p>
+        </li>
+        <li>
+            <h3>La loi de la durée</h3>
+            <p>Le marketing agit sur le long terme. Les actions marketing doivent se juger sur la durée - car ils prennent généralement une longue période de temps pour faire effet.</p>
+        </li>
+        <li>
+            <h3>La loi de l'extension</h3>
+            <p>Toute marque aura une irrésistible envie d'étendre son territoire et de mettre son nom sur toute sorte de produits ou de services. Les marques doivent résister à la tentation de se diluer dans d'autres territoires. A vouloir être tout pour tout le monde, vous finissez ne plus symboliser quoi que ce soit pour personne.</p>
+        </li>
+        <li>
+            <h3>La loi du sacrifice</h3>
+            <p>Pour gagner, il faut savoir se fixer des limites. Vous devrez par exemple abandonner des segments de marché qui ne font pas partie de votre cible, pour être plus efficace auprès de celle-ci.</p>
+        </li>
+        <li>
+            <h3>La loi des attributs</h3>
+            <p>À chaque attribut correspond un attribut opposé…aussi efficace. Par exemple : la direction assistée permet de d'améliorer le confort. Mais elle permet aussi d'améliorer la sécurité. Quelle attribut mettriez-vous en avant en tant que marque automobile ?</p>
+        </li>
+        <li>
+            <h3>La loi de la sincérité</h3>
+            <p>Une faute avouée est se transforme en vertu. Si votre produit n'est pas parfait, dites-le. Vous serez d'autant plus crédible. Avouer un défaut est un moyen efficace pour attirer l'attention des clients, si vous parvenez à en faire un atout.</p>
+        </li>
+        <li>
+            <h3>La loi de la singularité</h3>
+                <p>Dans chaque situation il n'existe qu'un angle d'approche qui permette des résultats significatifs. Si votre concurrent est vulnérable à un endroit, c'est là-dessus que vous devriez vous appuyer.</p>
+        </li>
+        <li>
+            <h3>La loi de l'imprévisible</h3>
+            <p>A moins que vous n'écriviez vous-même le plan d'action de vos concurrents, vous ne pouvez pas prévoir le futur. Changer n'est pas facile, mais c'est le seul moyen de faire face à l'imprévisible.</p>
+        </li>
+        <li>
+            <h3>La loi du succès</h3>
+            <p>Le succès rend arrogant et l'arrogance mène à l'échec. De nombreuses marques à succès ont disparues du jour au lendemain. Qui se souvient de Netscape, Polaroid ou Ericsson.</p>
+        </li>
+        <li>
+            <h3>La loi de l'échec</h3>
+            <p>Il faut vous attendre à échouer et accepter cela. L'échec est un passage obligé pour réussir. Ne vous fiez jamais à votre propre jugement pour savoir ce que veut le marché.</p>
+        </li>
+        <li>
+            <h3>La loi de la fièvre médiatique</h3>
+            <p>La situation est souvent contraire à ce qu'en disent les médias. Vous n'avez pas besoin de fièvre médiatique lorsque les choses vont bien. Quand vous en avez besoin, cela signifie que vous avez des problèmes.</p>
+        </li>
+        <li>
+            <h3>La loi de l'accélération</h3>
+            <p>Oubliez les modes. La seule manière d'avoir du succès, c'est de satisfaire le marché. Les initiatives les plus couronnées de succès sont bâties sur des tendances à long terme. Pas des modes.</p>
+        </li>
+        <li>
+            <h3>La loi des moyens</h3>
+            <p>Sans moyen adéquat, aucune idée ne peut réellement décoller. Il vous faudra des moyens financiers pour entrer dans la tête du client et y rester.</p>
+        </li>
+    </ol>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
